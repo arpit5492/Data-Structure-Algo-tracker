5485c69539ece054ee46dc0692dbfc2bafc92500
Arpit Mallick <arpit@Arpits-Air.lan>
2023-09-23 08:34:28 -0700

    Added insertion.py

---
+++ Arrays/insertion.py
@@ -0,0 +1,9 @@
+import array
+array1=array.array('i',[1,2,3,4])
+print(array1)
+array1.insert(0,6) #Inserting at the beginning
+print(array1)
+array1.insert(2,12) #Inserting at the middle
+print(array1)
+array1.insert(6,25) #Inserting at the end
+print(array1)
